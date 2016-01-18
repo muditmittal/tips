3dca57077013f6f75852dfc26512c2c71663b3f9
Mudit Mittal <mittal.mudit@gmail.com>
2016-01-17 23:21:44 -0800

    tips calculator
    this is for the prework of codepath iOS for Designers course.

--- tips/ViewController.swift
+++ tips/ViewController.swift
@@ -9,10 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var billAmount: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipPercent: UISegmentedControl!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        billAmount.becomeFirstResponder()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +25,24 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func onValueChange(sender: AnyObject) {
+        var bill = Double(billAmount.text!)
+        let tipValues = [0.10, 0.15, 0.20, 0.0]
+        let percent = tipValues[tipPercent.selectedSegmentIndex]
 
+        if bill == nil {
+            bill = 0.0
+        }
+
+        let tip = bill! * percent
+        let total = bill! + tip
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+    }
+
+    @IBAction func onTap(sender: AnyObject) {
+        view.endEditing(true)
+    }
 }
+
 
